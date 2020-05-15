ec4d29532955e50608a515d02e97dbac7b132fa2
Vishal Salgond <51847351+vishalsalgond@users.noreply.github.com>
2020-05-15 15:28:04 +0530

    Update rotateLL.cpp

--- Linked lists/rotateLL.cpp
+++ Linked lists/rotateLL.cpp
@@ -31,3 +31,41 @@ Node* rotate(Node* head, int k) {
     return new_head;
     
 }
+
+
+/*  This function should rotate list clockwise
+    by k and return new head (if changed) */
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode() : val(0), next(nullptr) {}
+ *     ListNode(int x) : val(x), next(nullptr) {}
+ *     ListNode(int x, ListNode *next) : val(x), next(next) {}
+ * };
+ */
+class Solution {
+public:
+    ListNode* rotateRight(ListNode* head, int k) {
+        if(!head || !head->next || k==0) return head;
+        ListNode *temp=head,*new_head;
+        int count=0;
+        while(temp){
+            temp = temp->next;
+            count++;
+        }
+        if(k%count==0) return head;
+        k = k%count;
+        temp = head;
+        for(int i=1;i<count-k;i++){
+            temp = temp->next;
+        }
+        new_head = temp -> next;
+        temp -> next = NULL;
+        temp = new_head;
+        while(temp->next) temp = temp-> next;
+        temp->next = head;
+        return new_head;
+    }
+};
