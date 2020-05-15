afd9c510068e8ece8cd3df5cf5799df60c2f907a
Vishal Salgond <51847351+vishalsalgond@users.noreply.github.com>
2020-05-15 15:47:17 +0530

    Create removenthNodeFromLast.cpp

---
+++ Linked lists/removenthNodeFromLast.cpp
@@ -0,0 +1,31 @@
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode() : val(0), next(nullptr) {}
+ *     ListNode(int x) : val(x), next(nullptr) {}
+ *     ListNode(int x, ListNode *next) : val(x), next(next) {}
+ * };
+ */
+class Solution {
+public:
+    ListNode* removeNthFromEnd(ListNode* head, int n) {
+        if(!head) return head;
+        int count=0;
+        ListNode *temp=head;
+        while(temp){
+            temp = temp->next;
+            count++;
+        }
+        if(n==count){
+            head = head ->next;
+            return head;
+        }
+        n = count-n-1;
+        temp = head;
+        while(n--) temp = temp->next;
+        temp ->next = temp -> next -> next;
+        return head;
+    }
+};
