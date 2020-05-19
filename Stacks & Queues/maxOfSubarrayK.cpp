b99b80323c7f8becf252b741ac1a0cf7ac0db7a7
Vishal Salgond <51847351+vishalsalgond@users.noreply.github.com>
2020-05-19 23:26:51 +0530

    Create maxOfSubarrayK.cpp

---
+++ Stacks & Queues/maxOfSubarrayK.cpp
@@ -0,0 +1,46 @@
+//https://practice.geeksforgeeks.org/problems/maximum-of-all-subarrays-of-size-k/0
+
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+	int t;
+	cin >> t;
+	while(t--){
+	    int n,k;
+	    cin >> n >>k;
+	    vector <int> a(n);
+	    deque <int> q;
+	    for(int i=0;i<n;i++) cin >> a[i];
+	    int i;
+	    //manually find max element for first k elements
+	    for(i=0;i<k;i++){
+	        //remove all elements smaller than curr
+	        while(!q.empty() && a[i]>=a[q.back()]){
+	            q.pop_back();
+	        }
+	        q.push_back(i);
+	    }
+	    for(;i<n;i++){
+	        //q.front will contain the max index of previous window
+	        cout << a[q.front()] << " ";
+	        
+	        //remove all elements of previous window
+	        while(!q.empty() && q.front()<=i-k) {
+	            q.pop_front();
+	            
+	        }
+	        //remove all elements that are smaller than current
+	        while(!q.empty() && a[i]>=a[q.back()]){
+	            q.pop_back();
+	            
+	        }
+	        //add index of current element;
+	        q.push_back(i);
+	    }
+	    //print max element of last window
+	    cout << a[q.front()] << endl;
+	}
+	
+	return 0;
+}
