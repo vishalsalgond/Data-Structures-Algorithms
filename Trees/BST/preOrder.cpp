230144a68931c0add618eae3692ff569dafc2877
Vishal Salgond <51847351+vishalsalgond@users.noreply.github.com>
2020-05-24 16:50:12 +0530

    Create preOrder.cpp

---
+++ Trees/BST/preOrder.cpp
@@ -0,0 +1,44 @@
+//https://practice.geeksforgeeks.org/problems/preorder-traversal/1
+
+/* A binary tree node has data, pointer to left child
+   and a pointer to right child  
+
+/*
+struct Node
+{
+    int data;
+    struct Node* left;
+    struct Node* right;
+    
+    Node(int x){
+        data = x;
+        left = right = NULL;
+    }
+};
+*/
+
+//recursive approach
+void preorder(Node* root)
+{
+    if(!root) return;
+    cout << root->data << " ";
+    preorder(root->left);
+    preorder(root->right);
+}
+
+
+//iterative approach
+void preorder(Node* root)
+{
+    stack <Node *> tree;
+    tree.push(root);
+    while(!tree.empty()){
+        Node *temp = tree.top();
+        tree.pop();
+        if(temp){
+            cout << temp->data << " ";
+            tree.push(temp->right);
+            tree.push(temp->left);   
+        }
+    }
+}
