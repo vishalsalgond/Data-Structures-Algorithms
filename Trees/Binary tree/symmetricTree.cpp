0dacfc6ab1d37fa279b708e6697475304d625b70
Vishal Salgond <51847351+vishalsalgond@users.noreply.github.com>
2020-06-28 18:03:29 +0530

    Update symmetricTree.cpp

--- Trees/Binary tree/symmetricTree.cpp
+++ Trees/Binary tree/symmetricTree.cpp
@@ -1,5 +1,7 @@
 //https://leetcode.com/problems/symmetric-tree/
 
+
+//Recursive
 /**
  * Definition for a binary tree node.
  * struct TreeNode {
@@ -29,5 +31,59 @@ public:
         if(root==NULL) return true;
         
         return dfs(root->left, root->right);
+    }
+};
+
+
+
+//Iterative
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+class Solution {
+    
+public:
+    bool isSymmetric(TreeNode* root) {
+        
+        if(root==NULL) return true;
+        
+        queue <TreeNode*> Q;
+        Q.push(root);
+        Q.push(root);
+        
+        while(!Q.empty()){
+            TreeNode *leftNode = Q.front();
+            Q.pop();
+            TreeNode *rightNode = Q.front();
+            Q.pop();
+            
+            if(!leftNode && !rightNode) continue;
+            
+            else if (!leftNode || !rightNode) return false;
+            
+            else if(leftNode->val != rightNode->val) return false;
+            
+            else{
+                Q.push(leftNode->left);
+                Q.push(rightNode->right);
+                
+                Q.push(leftNode->right);
+                Q.push(rightNode->left);
+            }
+            
+        }
+        
+        return true;
+        
+        
+        
     }
 };
