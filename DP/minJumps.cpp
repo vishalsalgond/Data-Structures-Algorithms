b10c5f1bf2145eb98e00b0878a01c987f0ced46b
Vishal Salgond <51847351+vishalsalgond@users.noreply.github.com>
2020-07-15 20:22:43 +0530

    Update minJumps.cpp

--- DP/minJumps.cpp
+++ DP/minJumps.cpp
@@ -1,13 +1,47 @@
 //https://leetcode.com/problems/jump-game-ii/
 
+//use optimal substructure property
+//min jumps to reach i is
+//min jumps to reach i - x plus
+//min jums to reach x from i - x
+
+//Time O(n*n)
+//Space O(n)
+
 class Solution {
 public:
     int jump(vector<int>& nums) {
-        //use optimal substructure property
-        //min jumps to reach i is
-        //min jumps to reach i - x plus
-        //min jums to reach x from i - x
+     
+        int n = nums.size();
+        int dp[n];
         
+        dp[0] = 0;
+        
+        for(int i = 1; i < n; i++){
+            dp[i] = INT_MAX;
+        }
+        
+        for(int i = 1; i < n; i++){
+            for(int j = 0; j < i; j++){
+                if(nums[j] + j >= i){
+                    dp[i] = min(dp[i], 1 + dp[j]);
+                    break;
+                }
+            }
+        }
+        
+        return dp[n-1];
+    }
+};
+
+
+//TIme O(n)
+//Space O(1)
+
+class Solution {
+public:
+    int jump(vector<int>& nums) {
+
         int n = nums.size();
         
         if(nums[0] == 0 || n == 1){
