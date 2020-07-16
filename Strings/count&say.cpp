cd54a5cb995ffa5c005164bd67ec6d0906fe0a7b
Vishal Salgond <51847351+vishalsalgond@users.noreply.github.com>
2020-07-16 13:25:00 +0530

    Create count&say.cpp

---
+++ Strings/count&say.cpp
@@ -0,0 +1,23 @@
+//https://leetcode.com/problems/count-and-say/
+
+class Solution {
+public:
+    string countAndSay(int n) {
+        if(n == 0) return "";
+        string res = "1";
+        
+        while(--n){
+            string curr = "";
+            for(int i = 0; i < res.size(); i++){
+                int count = 1;
+                while((i+1) < res.size() && res[i] == res[i+1]){
+                    count++;
+                    i++;
+                }
+                curr += to_string(count) + res[i];
+            }
+            res = curr;
+        }
+        return res;
+    }
+};
