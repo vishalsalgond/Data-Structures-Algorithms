1ee3cc5855b6183e698f72f05ee391939c9899df
Vishal Salgond <51847351+vishalsalgond@users.noreply.github.com>
2020-07-27 18:11:22 +0530

    Create maxSubarrayWith0&1.cpp

---
+++ Arrays/maxSubarrayWith0&1.cpp
@@ -0,0 +1,25 @@
+//https://practice.geeksforgeeks.org/problems/largest-subarray-of-0s-and-1s/1
+
+int maxLen(int arr[], int n)
+{
+    int dp[n+1];
+    map<int, int> mp;
+    
+    for(int i = 0; i <= n; i++) {
+        if(arr[i] == 0) 
+            arr[i] = -1;
+    }
+    
+    int c_sum = 0 , ans = 0;
+    mp[0] = 0;
+    for(int i = 0; i < n; i++) {
+        c_sum += arr[i];
+        if(c_sum != 0 && mp[c_sum] == 0) {
+            mp[c_sum] = i + 1;
+        }
+        ans = max(ans, i + 1 - mp[c_sum]);
+    }
+    
+    return ans;
+    
+}
