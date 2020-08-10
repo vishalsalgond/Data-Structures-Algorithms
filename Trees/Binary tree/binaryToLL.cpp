5ca1132afe5095654c98661cc4df88bde9f40cb3
Vishal Salgond <51847351+vishalsalgond@users.noreply.github.com>
2020-08-10 22:51:59 +0530

    Create binaryToLL.cpp

---
+++ Trees/Binary tree/binaryToLL.cpp
@@ -0,0 +1,35 @@
+//https://leetcode.com/problems/flatten-binary-tree-to-linked-list/
+
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+class Solution {
+public:
+    void flatten(TreeNode* root) {
+        if(!root) return;
+        
+        //recursively shift left subtree to right subtree
+        flatten(root -> left);
+        flatten(root -> right);
+        
+        if(root -> left) {
+            
+            TreeNode* right = root -> right;
+            
+            root -> right = root -> left;
+            root -> left = NULL;
+            
+            while(root -> right) root = root -> right;
+            
+            root -> right = right;
+        }
+    }
+};
