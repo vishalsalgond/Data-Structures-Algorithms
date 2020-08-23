2a5d64aae284d923cfc33d96f06c1999a0c68056
Vishal Salgond <51847351+vishalsalgond@users.noreply.github.com>
2020-08-23 15:55:40 +0530

    Create floyedWarshall.cpp

---
+++ Graph/floyedWarshall.cpp
@@ -0,0 +1,85 @@
+#include<bits/stdc++.h>
+using namespace std;
+#define gc getchar_unlocked
+#define fo(i,n) for(i=0;i<n;i++)
+#define Fo(i,k,n) for(i=k;k<n?i<n:i>n;k<n?i+=1:i-=1)
+#define ll long long
+#define si(x) scanf("%d",&x)
+#define sl(x) scanf("%lld",&x)
+#define ss(s) scanf("%s",s)
+#define pi(x) printf("%d\n",x)
+#define pl(x) printf("%lld\n",x)
+#define ps(s) printf("%s\n",s)
+#define deb(x) cout << #x << "=" << x << endl
+#define deb2(x, y) cout << #x << "=" << x << "," << #y << "=" << y << endl
+#define pb push_back
+#define mp make_pair
+#define F first
+#define S second
+#define all(x) x.begin(), x.end()
+#define clr(x) memset(x, 0, sizeof(x))
+#define sortall(x) sort(all(x))
+#define tr(it, a) for(auto it = a.begin(); it != a.end(); it++)
+#define PI 3.1415926535897932384626
+typedef pair<int, int>  pii;
+typedef pair<ll, ll>  pl;
+typedef vector<int>   vi;
+typedef vector<ll>    vl;
+typedef vector<pii>   vpii;
+typedef vector<pl>    vpl;
+typedef vector<vi>    vvi;
+typedef vector<vl>    vvl;
+//========================================
+#define V 4
+#define INF 99999
+
+void printSolution(int dist[][V])  
+{  
+    cout<<"The following matrix shows the shortest distances"
+            " between every pair of vertices \n";  
+    for (int i = 0; i < V; i++)  
+    {  
+        for (int j = 0; j < V; j++)  
+        {  
+            if (dist[i][j] == INF)  
+                cout<<"INF"<<"     ";  
+            else
+                cout<< dist[i][j] <<"     ";  
+        }  
+        cout<<endl;  
+    }  
+}
+
+void floydWarshall(int graph[][V]) {
+    int dist[V][V] = {0};
+
+    for(int i = 0; i < V; i++) {
+        for(int j = 0; j < V; j++) {
+            dist[i][j] = graph[i][j];
+        }
+    }
+
+    for(int k = 0; k < V; k++) {
+        for(int i = 0; i < V; i++) {
+            for(int j = 0; j < V; j++) {
+                if(dist[i][k] + dist[k][j] < dist[i][j])
+                    dist[i][j] = dist[i][k] + dist[k][j];
+            }
+        }
+    }
+    printSolution(dist);
+}
+  
+
+int main() {
+    ios_base::sync_with_stdio(0);
+    int graph[V][V] = { {0, 5, INF, 10},  
+                        {INF, 0, 3, INF},  
+                        {INF, INF, 0, 1},  
+                        {INF, INF, INF, 0}  
+                    };  
+   
+    floydWarshall(graph);  
+    return 0;
+}
+
